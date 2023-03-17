19a0b5c6cd04168ec99b3bf8f9a449a984bb8c4a
hosseinmohammadi <hosseinmohammaditr@gmail.com>
2023-03-17 15:00:14 +0300

    first commit

---
+++ ssh-copy-key.sh
@@ -0,0 +1,146 @@
+# !/usr/bin/env bash
+
+###### USAGE ######
+function _usage(){
+	echo "Usage: 
+	-c --create	- create a new ssh with given name
+	-n --name	- exist ssh-key filename
+	-p --pass	- hosts passwords
+	-i --host	- hostsnames or ips
+	-u --user	- usernames
+	"
+}
+
+## create ssh-key
+function _create_ssh_key(){
+	echo "creating ssh ..."
+
+	#  -N new_passphrase provides the new passphrase.
+	#  -q                silence ssh-keygen.
+	#  -f filename       specifies the filename of the key file.
+	ssh-keygen -q -t rsa -N '' -f $HOME/.ssh/$1 <<<y >/dev/null 2>&1
+}
+
+### copy ssh-key
+function _cp_ssh_key(){
+
+	echo "copying ssh keys.pub ..."
+	IFS=','
+	read -ra users <<< "$1"
+	read -ra hosts <<< "$2"
+	read -ra pass <<< "$3"
+
+	#echo "${#hosts[@]}, ${hosts[@]}"
+	#echo "${#pass[@]}, ${pass[@]}"
+	#echo "${#users[@]}, ${users[@]}"
+
+	## single user - multiple pass
+	if [[ ${#users[@]} -eq 1 && ${#pass[@]} -gt 1 && ${#pass[@]} -eq ${#hosts[@]} ]]; then
+		
+		echo "single user multiple pass"
+		
+		i=0
+		while [ $i -lt ${#hosts[@]} ]; do
+			echo "sshpass -p ${pass[$i]} ssh-copy-id -i /$HOME/.ssh/$4.pub -f -o StrictHostKeyChecking=no ${users[0]}@${hosts[$i]}"
+			i=$(( $i + 1 ))
+		done
+	
+	## single user/pass
+	elif [[ ${#users[@]} -eq 1 && ${#pass[@]} -eq 1 ]]; then
+		
+		echo "single user/pass"
+		for host in "${hosts[@]}"; do
+			sshpass -p ${pass[0]} ssh-copy-id -i /$HOME/.ssh/$4.pub -f -o StrictHostKeyChecking=no ${users[0]}@$host
+		done
+	
+	
+	## multiple user/pass
+	elif [[ ${#users[@]} -gt 1 && ${#pass[@]} -gt 1 && ${#pass[@]} -eq ${#hosts[@]} ]]; then
+	
+		echo "mutliple user/pass"
+		i=0
+		while [ $i -lt ${#hosts[@]} ]; do
+		
+			sshpass -p ${pass[$i]} ssh-copy-id -i /$HOME/.ssh/$4.pub -f -o StrictHostKeyChecking=no ${users[$i]}@${hosts[$i]}
+			i=$(( $i + 1 ))	
+		done
+	
+	### single pass multiple users
+	elif [[ ${#users[@]} -gt 1 && ${#pass[@]} -eq 1 && ${#users[@]} -eq ${#hosts[@]} ]]; then
+		
+		echo "single pass multiple users"
+		i=0
+		while [ $i -lt ${#hosts[@]} ]; do
+			sshpass -p ${pass[0]} ssh-copy-id -i /$HOME/.ssh/$4.pub -f -o StrictHostKeyChecking=no ${users[$i]}@${hosts[$i]}
+			i=$(( $i + 1 ))
+		done
+	
+
+	else
+		_usage
+		exit 1
+	fi
+}
+
+#echo "my params -> $@"
+for param in "$@"; do
+	shift
+	case "$param" in
+		"--help") set -- "$@" "-h" ;;
+		"--create") set -- "$@" "-c" ;;
+		"--pass") set -- "$@" "-p" ;;
+		"--host") set -- "$@" "-i" ;;
+		"--user") set -- "$@" "-u" ;;
+		*) set -- "$@" "$param" ;;
+
+	esac
+done
+
+#echo "my params -> $@"
+
+OPTIND=1
+while getopts ":c:h:u:i:p:" opt; do
+	case "$opt" in
+		h)
+			_usage
+			exit 0
+			;;
+		c)
+			create_ssh_key=true
+			key_name=${OPTARG}
+			;;
+		i)
+			hosts=true; ips=${OPTARG}
+			;;
+		p)
+			pass=true
+			passwords=${OPTARG}
+			;;
+		u)
+			user=true
+			users=${OPTARG}
+			;;
+		?)
+			_usage >&2
+			exit 1
+			;;
+	esac
+done
+
+#echo "ssh key filename ->	$key_name"
+#echo "ips ->			$ips"
+#echo "pass ->			$passwords"
+
+shift $((OPTIND - 1))
+
+if [[ -n "${create_ssh_key}" ]]; then 
+	_create_ssh_key $key_name
+fi
+
+
+if [[ -n "${hosts}" && -n "${pass}" && -n "${user}" ]]; then
+	_cp_ssh_key $users $ips $passwords $key_name
+else
+	_usage >&2
+	exit 1
+fi
